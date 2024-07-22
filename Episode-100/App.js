6b9826622df47e08b5960e842a579b6462866c5e
striverzx <striverzx011@gmail.com>
2024-07-22 22:24:21 +0530

    new files are updated

---
+++ Episode-100/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading=React.createElement("h1",{},"Namaste-React");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
