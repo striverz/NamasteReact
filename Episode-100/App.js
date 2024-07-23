cff8412e3879b4002d0a0d39cb01ce36fd5edd33
striverzx <striverzx011@gmail.com>
2024-07-23 18:54:15 +0530

    app is updated

--- Episode-100/App.js
+++ Episode-100/App.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading=React.createElement("h1",{},"Namaste-React");
+const heading=<h1>React Element</h1>;
+
+const Title=()=>{
+    return(
+        <h1>Namaste React🚀 </h1>
+    )
+}
+const HeaderComponent =()=>{
+    return  (
+        <>
+            <h1>React Functional component</h1>
+            {heading}
+            <Title/>
+        </>
+    )
+    
+}
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeaderComponent/>);
